99b4a9434fe371cb341b957a4d77d73cbdc3f18a
TakanawaYuku <yukutakanawa@gmail.com>
2023-01-23 20:25:39 +0300

    changes code api

--- js/api/Yandex.js
+++ js/api/Yandex.js
@@ -4,13 +4,13 @@
  * Имеет свойство HOST
  * */
 class Yandex {
-  static HOST = 'https://cloud-api.yandex.net/v1/disk';
+  static HOST = 'https://cloud-api.yandex.net:443/v1/disk';
 
   /**
    * Метод формирования и сохранения токена для Yandex API
    */
   static getToken(){
-    let token = localStorage.getItem('yandex_token');
+	  let token = localStorage.getItem('yandex_token');
 	  if (token==null){
 		  let res = null;
 		  while (res == null){
@@ -26,48 +26,48 @@ class Yandex {
    * Метод загрузки файла в облако
    */
   static uploadFile(path, url, callback){
-    	  // path - КУДА НА ЯНДЕКС.ДИСКЕ
+	  // path - КУДА НА ЯНДЕКС.ДИСКЕ
 	  //
 	  // url - местоположение картинки
 	  createRequest({
-      method: "POST",
-      url: Yandex.HOST+"/resources/upload/?path="+encodeURIComponent(path)+"&url="+encodeURIComponent(url),
-      auth:"OAuth "+Yandex.getToken(),
-      callback: callback,
-      });
+		method: "POST",
+		url: Yandex.HOST+"/resources/upload/?path="+encodeURIComponent(path)+"&url="+encodeURIComponent(url),
+		auth:"OAuth "+Yandex.getToken(),
+		callback: callback,
+	  });
   }
 
   /**
    * Метод удаления файла из облака
    */
   static removeFile(path, callback){
-    createRequest({
-      method: "DELETE",
-      url: Yandex.HOST+"/resources?path="+encodeURIComponent(path),
-      auth:"OAuth "+Yandex.getToken(),
-      callback: callback,
-    });
+	createRequest({
+		method: "DELETE",
+		url: Yandex.HOST+"/resources?path="+encodeURIComponent(path),
+		auth:"OAuth "+Yandex.getToken(),
+		callback: callback,
+	});
   }
 
   /**
    * Метод получения всех загруженных файлов в облаке
    */
   static getUploadedFiles(callback){
-    createRequest({
-      method: "GET",
-      //url: Yandex.HOST+"/resources/files?limit=1000",
-      url: Yandex.HOST+"/resources/last-uploaded?limit=5&media_type=image",
-      auth:"OAuth "+Yandex.getToken(),
-      callback: callback,
-    });
+	createRequest({
+		method: "GET",
+		//url: Yandex.HOST+"/resources/files?limit=1000",
+		url: Yandex.HOST+"/resources/last-uploaded?limit=5&media_type=image",
+		auth:"OAuth "+Yandex.getToken(),
+		callback: callback,
+	});
   }
 
   /**
    * Метод скачивания файлов
    */
   static downloadFileByUrl(url){
-    const link = document.createElement('a');
-	  link.href=url;
+	const link = document.createElement('a');
+	link.href=url;
 	  link.onclick = () =>{
 		location.href = url;
 	  };
