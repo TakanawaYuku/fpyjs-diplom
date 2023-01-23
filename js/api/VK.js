da487021dbdabc73ca774fa2876ece341b7b0c40
TakanawaYuku <yukutakanawa@gmail.com>
2023-01-23 19:39:28 +0300

    add work vk

--- js/api/VK.js
+++ js/api/VK.js
@@ -13,14 +13,52 @@ class VK {
    * Получает изображения
    * */
   static get(id = '', callback){
+    VK.lastCallback = callback;
 
+    const script = document.createElement('SCRIPT');
+      const url =
+      "https://api.vk.com/method/photos.get?"+
+      "owner_id="+id+
+      "&album_id=profile"+
+      "&access_token="+this.ACCESS_TOKEN+
+      "&v=5.131&callback=VK."+VK.processData;
+
+    script.src = url.slice(0,url.indexOf('('));
+
+    document.getElementsByTagName("head")[0].appendChild(script);
+    }
+
+    /**
+     * Передаётся в запрос VK API для обработки ответа.
+     * Является обработчиком ответа от сервера.
+     */
+    static processData(result){
+    try {
+      const script = document.getElementsByTagName("head")[0].lastChild;
+      script.remove();
+
+      /*
+      Найдите самые крупные изображения из ответа от сервера и передайте
+      изображения в колбек,
+      который передавался в метод VK.get, который сохранялся в lastCallback.
+      Обновите свойство lastCallback на функцию "пустышку" () => {}.
+      */
+      const photoArray = result.response.items;
+      const photoResults = [];
+      photoArray.forEach((photo) =>{
+        const sizes = [];
+        for (const i of photo.sizes){
+          sizes.push(Number(i.height));
+        }
+        const indexMax = sizes.indexOf(Math.max(...sizes));
+        photoResults.push(photo.sizes[indexMax].url)
+      });
+      VK.lastCallback(photoResults);
+
+      VK.lastCallback = () =>{};
+    }
+    catch (e) {
+      alert(`${e.name}: ${e.message}`);
+    }
+    }
   }
-
-  /**
-   * Передаётся в запрос VK API для обработки ответа.
-   * Является обработчиком ответа от сервера.
-   */
-  static processData(result){
-
-  }
-}
