ba1cb26b0afbb3757e4066f64e80a0234b9397dd
TakanawaYuku <yukutakanawa@gmail.com>
2023-01-23 19:30:17 +0300

    add func request yandex api

--- js/api/createRequest.js
+++ js/api/createRequest.js
@@ -2,5 +2,23 @@
  * Основная функция для совершения запросов по Yandex API.
  * */
 const createRequest = (options = {}) => {
+    const xhr = new XMLHttpRequest;
 
+    try {
+         xhr.addEventListener('readystatechange', (e)=> {
+            e.preventDefault();
+            if (xhr.readyState === xhr.DONE) {
+                options.callback(xhr);
+            }
+         });
+
+         xhr.open(options.method, options.url);
+         xhr.responseType = 'json';
+         xhr.setRequestHeader('Authorization', options.auth);
+         xhr.setRequestHeader('content-type', 'application/json');
+         xhr.send();
+    }
+    catch (error) {
+        console.log(error)
+    }
 };
